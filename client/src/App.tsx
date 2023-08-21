bc7bcbe186173329dbcb1c305bc67f50ab5e1f15
Xaviour Greenhalgh <xaviourgreenhalgh@gmail.com>
2023-08-21 20:33:15 +1000

    broken firebase integration

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,19 @@ import SecurityStrategyAuditPage from './pages/SecurityStrategyAuditPage';
 import DefaultTheme from './core/Theme';
 import TopNav from './components/navigation/TopNav';
 
+//=============================================FIREBASE---
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyBio07tNYYBLLr4n1cePUHeAE6nuxyxuvI",
+  authDomain: "avertro-challenge.firebaseapp.com",
+  projectId: "avertro-challenge",
+  storageBucket: "avertro-challenge.appspot.com",
+  messagingSenderId: "803030593604",
+  appId: "1:803030593604:web:f29fd72a117632eed98c11"
+};
+const app = initializeApp(firebaseConfig);
+//=============================================FIREBASE^^^
+
 function App() {
   return (
     <ThemeProvider theme={DefaultTheme}>
