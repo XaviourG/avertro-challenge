bbf4a2adc42291b0155738d59332d8c150fd4ccb
Xaviour Greenhalgh <xaviourgreenhalgh@gmail.com>
2023-08-21 18:59:22 +1000

    configure favicon and app title to match avertro site:

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,3 @@
-import './App.css';
 import SecurityStrategyAuditPage from './pages/SecurityStrategyAuditPage';
 
 function App() {
--- client/src/core/ColorPalette.ts
+++ client/src/core/ColorPalette.ts
@@ -3,20 +3,13 @@ interface ColorsMap {
 }
 
 class Colors implements ColorsMap {
-  // page structure
   static readonly AVERTRO_BACKGROUND = '#F8F8F8';
   static readonly AVERTRO_WHITE = '#FFFFFF';
-
-  // neutrals
-  static readonly PAGE_BORDER = '#D7D7D7';
-  static readonly TEXT_BORDER = '#BDBDBD';
-
-  // text
-  static readonly BODY_TEXT = '#000000';
-
-  // brand colors
   static readonly AVERTRO_BLUE = '#25397D';
   static readonly AVERTRO_RED = '#E03345';
+  static readonly PAGE_BORDER = '#D7D7D7';
+  static readonly TEXT_BORDER = '#BDBDBD';
+  static readonly BODY_TEXT = '#000000';
 
   [colorName: string]: string;
 }
