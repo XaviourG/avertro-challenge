bc83d6ea11ce312ad6d9128761039dd369089cd5
Xaviour Greenhalgh <xaviourgreenhalgh@gmail.com>
2023-08-21 19:34:46 +1000

    style top navigation bar

--- client/src/components/navigation/TopNav.tsx
+++ client/src/components/navigation/TopNav.tsx
@@ -1,8 +1,19 @@
 import { ReactElement } from "react"
 import AvertroLogo from "./AvertroLogo"
+import Colors from "../../core/ColorPalette";
 
 const TopNav = (): ReactElement =>
-  <div>
+  <div style={{
+    width: '100vw',
+    backgroundColor: Colors.AVERTRO_WHITE,
+    paddingTop: '0.825rem',
+    paddingBottom: '0.75rem',
+    display: 'flex',
+    justifyContent: 'center',
+    boxShadow: '0px 4px 10px 0px #0000000A',
+    position: 'sticky',
+    top: 0,
+  }}>
     <a href="https://www.avertro.com/">
       <AvertroLogo/>
     </a>
