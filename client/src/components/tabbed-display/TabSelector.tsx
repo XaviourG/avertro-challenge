4dd49cdfa23fb87f5560f9a1c627cfcbec84d469
Xaviour Greenhalgh <xaviourgreenhalgh@gmail.com>
2023-08-22 16:22:20 +1000

    refactor tab selectors to minimise on mobile

--- client/src/components/tabbed-display/TabSelector.tsx
+++ client/src/components/tabbed-display/TabSelector.tsx
@@ -1,5 +1,5 @@
 import { ReactElement } from "react";
-import { Typography } from "@mui/material";
+import { Typography, useMediaQuery, useTheme } from "@mui/material";
 import Colors from "../../core/ColorPalette";
 
 interface Props {
@@ -13,29 +13,67 @@ const TabSelector = ({
   focusTab,
   setFocusTab
 }: Props): ReactElement => {
-  return <div style={{
-    width: '100%',
-    display: 'flex',
-    justifyContent: 'start',
-  }}>
-    {
-      tabTitles.map((title, key) => <div key={key} style={{
-        cursor: 'pointer',
-        borderRadius: '10px 10px 0px 0px',
-        backgroundColor:`${
-          focusTab === key ? Colors.AVERTRO_WHITE : Colors.PAGE_BORDER
-        }`,
-        padding: '0.5rem 1.5rem 0.5rem 1.5rem',
-        transition: '500ms',
-      }} onClick={() => setFocusTab(key)}>
-        <Typography variant="h2" sx={{
-          m: '0',
-        }}>
-          {title}
-        </Typography>
-      </div>)
-    }
-  </div>
-}
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('md'));
+
+  if (isMobile) {
+    const minimalTabs = tabTitles.map((title) => {
+      const words = title.split(' ');
+      const initals = words.map((word) => (word.length >= 1 ? word[0] : ''));
+      return initals.join('');
+    })
+    return (
+      <div style={{
+        width: '100%',
+        display: 'flex',
+        justifyContent: 'start',
+      }}>
+        {
+          minimalTabs.map((title, key) => <div key={key} style={{
+            cursor: 'pointer',
+            borderRadius: '10px 10px 0px 0px',
+            backgroundColor:`${
+              focusTab === key ? Colors.AVERTRO_WHITE : Colors.PAGE_BORDER
+            }`,
+            padding: '0.5rem 1.5rem 0.5rem 1.5rem',
+            transition: '500ms',
+          }} onClick={() => setFocusTab(key)}>
+            <Typography variant="h2" sx={{
+              m: '0',
+            }}>
+              {title}
+            </Typography>
+          </div>)
+        }
+      </div>
+    );
+  }
+
+  return (
+    <div style={{
+      width: '100%',
+      display: 'flex',
+      justifyContent: 'start',
+    }}>
+      {
+        tabTitles.map((title, key) => <div key={key} style={{
+          cursor: 'pointer',
+          borderRadius: '10px 10px 0px 0px',
+          backgroundColor:`${
+            focusTab === key ? Colors.AVERTRO_WHITE : Colors.PAGE_BORDER
+          }`,
+          padding: '0.5rem 1.5rem 0.5rem 1.5rem',
+          transition: '500ms',
+        }} onClick={() => setFocusTab(key)}>
+          <Typography variant="h2" sx={{
+            m: '0',
+          }}>
+            {title}
+          </Typography>
+        </div>)
+      }
+    </div>
+  );
+};
 
 export default TabSelector;
