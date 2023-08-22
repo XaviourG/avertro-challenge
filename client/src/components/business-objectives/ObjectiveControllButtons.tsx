571f989829fcb58bbbde442666d1ab7d04d44d28
Xaviour Greenhalgh <xaviourgreenhalgh@gmail.com>
2023-08-22 19:25:49 +1000

    refactor update button to simulate loading

--- client/src/components/business-objectives/ObjectiveControllButtons.tsx
+++ client/src/components/business-objectives/ObjectiveControllButtons.tsx
@@ -1,5 +1,5 @@
-import { Button, Grid, Typography } from "@mui/material";
-import { ReactElement } from "react";
+import { Button, CircularProgress, Grid, Typography } from "@mui/material";
+import { ReactElement, useState } from "react";
 import Colors from "../../core/ColorPalette";
 
 interface Props {
@@ -9,6 +9,13 @@ interface Props {
 const ObjectiveControllButtons = ({
   removeForm,
 }: Props): ReactElement => {
+  const [updating, setUpdating] = useState(false);
+
+  const simulateUpdate = async () => {
+    setUpdating(true);
+    await new Promise(delay => setTimeout(delay, 1000));
+    setUpdating(false);
+  }
 
   return (
     <Grid item xs={12} pr={{ xs: '1.5rem', lg: '0rem'}}>
@@ -36,17 +43,28 @@ const ObjectiveControllButtons = ({
           ml: '1.75rem',
           py: '0.75rem',
           px: '1.25rem',
+          width: '6rem',
           textTransform: 'none',
           boxShadow: 'none',
           borderRadius: '5px',
-        }}>
-          <Typography sx={{
-            fontFamily: 'Inter',
-            fontWeight: 500,
-            color: Colors.AVERTRO_WHITE,
-          }}>
-            Update
-          </Typography>
+          color: Colors.AVERTRO_WHITE
+        }} onClick={simulateUpdate}>
+          {
+            updating
+            ?
+            <CircularProgress
+              size={'1rem'}
+              color={'inherit'}
+            />
+            :
+            <Typography sx={{
+              fontFamily: 'Inter',
+              fontWeight: 500,
+              color: Colors.AVERTRO_WHITE,
+            }}>
+              Update
+            </Typography>
+          }
         </Button>
       </div>
     </Grid>
